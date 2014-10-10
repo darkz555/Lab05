ee50ffbd166ce9ca2220bb25650024686599a435
Allan Baños <Allan@MacBook-Pro-de-Allan.local>
2014-10-09 19:46:56 -0500

    Initial Commit

---
+++ Lab05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab05
+//
+//  Created by Allan Baños on 09/10/14.
+//  Copyright (c) 2014 Edgar Allan Baños Romero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
